1847e5ff71c8ea2cdcb5167531a1f026c611d5ce
XLR8tomi <reach.464@yahoo.com>
2022-05-18 09:53:14 -0500

    w

---
+++ 1.py
@@ -0,0 +1,6 @@
+1
+
+age = input("How old are you " + First_Name + "? ")
+print("You are " + str(age) + " years old")
+height = input("What is your height in cm? ")
+print("Your height is " + str(height) + "cm")
---
+++ test.py/Untitled-2.py
@@ -0,0 +1,10 @@
+First_Name = input("What is your first name? ")
+Last_Name = input("What is your last name? ")
+print("Hello", First_Name +" "+ Last_Name)
+
+
+
+age = input("How old are you " + First_Name + "? ")
+print("You are " + str(age) + " years old")
+height = input("What is your height in cm? ")
+print("Your height is " + str(height) + "cm")
