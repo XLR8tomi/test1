a78f627f8d9eea6093b8a605e80140a87706db87
XLR8tomi <reach.464@yahoo.com>
2022-05-19 10:38:51 -0500

    w

--- test.py/Untitled-2.py
+++ test.py/Untitled-2.py
@@ -4,7 +4,7 @@ print("Hello", First_Name +" "+ Last_Name)
 
 
 
-age = input("How old are you " + First_Name + "? ")
+age = int(input("How old are you " + First_Name + "? "))
 print("You are " + str(age) + " years old")
 height = input("What is your height in cm? ")
 print("Your height is " + str(height) + "cm")
